1a35c832dd6c5582d792f88eef7d40a36b21468a
aklump <sourcecode@intheloftstudios.com>
2015-11-19 07:56:37 -0800

    Version bumped from 0.1 to 0.1.1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wait For (an Element) jQuery Plugin
+
+You need this plugin if you are trying to do something AFTER a certain element has appeared on the page, such as when you are creating elements via Javascript.  If you're using another library that does not fire a callback when the element appears, you can use this plugin to know when that element is ready on the page to do further processing.
+
+See `jquery.wait_for.js` for usage info
--- jquery.wait_for.js
+++ jquery.wait_for.js
@@ -1,5 +1,5 @@
 /**
- * Wait For (an Element) jQuery JavaScript Plugin v0.1
+ * Wait For (an Element) jQuery JavaScript Plugin v0.1.1
  * http://www.intheloftstudios.com/packages/js/wait_for
  *
  * Wait for an element to be ready on a page and fire a callback.
@@ -7,7 +7,7 @@
  * Copyright 2015, Aaron Klump <sourcecode@intheloftstudios.com>
  * @license Dual licensed under the MIT or GPL Version 2 licenses.
  *
- * Date: Thu Nov 19 07:50:27 PST 2015
+ * Date: Thu Nov 19 07:56:29 PST 2015
  */
 
 /**
@@ -92,6 +92,6 @@ $.fn.waitFor = function() {
  *
  * @return {string}
  */
-$.fn.waitFor.version = function() { return '0.1'; };
+$.fn.waitFor.version = function() { return '0.1.1'; };
 
 })(jQuery, window);
