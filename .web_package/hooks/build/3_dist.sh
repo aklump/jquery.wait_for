3a888b029a19a86e07bb565a935ae3315ae5f03b
Aaron Klump <sourcecode@intheloftstudios.com>
2017-05-18 10:53:51 -0700

    update build.

--- .web_package/hooks/build/3_dist.sh
+++ .web_package/hooks/build/3_dist.sh
@@ -3,7 +3,14 @@
 # @file
 # Copy distribution files to /dist
 # 
-sleep 3
+# Allow time for all CodeKit to minify.
+while [ ! -f "$7/jquery.wait_for.min.js" ]; do
+  sleep 1
+done
+
+test -h "$7/dist" && rm "$7/dist"
 test -d "$7/dist" || mkdir -p "$7/dist"
+
 cp "$7/jquery.wait_for.js" "$7/dist/"
 cp "$7/jquery.wait_for.min.js" "$7/dist/"
+cp "$7/jquery.wait_for.min.js.map" "$7/dist/"
